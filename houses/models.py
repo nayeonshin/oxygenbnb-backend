0b49dacff6ee27618152d923ac2b7bf50419bc23
Nayeon Shin <dev.nayeon@gmail.com>
2023-11-07 21:14:26 -0500

    feat: migrate `Houses`

--- houses/models.py
+++ houses/models.py
@@ -5,5 +5,5 @@ class House(models.Model):
     """Model definition for Houses"""
     name = models.CharField(max_length=140)
     price = models.PositiveIntegerField()
-    description = models.TextField
+    description = models.TextField(null=True)
     address = models.CharField(max_length=140)
